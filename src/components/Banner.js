c723cca7e116843ae5bc58e006fdd7424f4ce1f4
Ben Holgate <holgateb@hotmail.com>
2022-11-23 15:05:43 -0800

    added banner image.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.png";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
 
@@ -93,11 +94,16 @@ export const Banner = () => {
               )}
             </TrackVisibility>
           </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img"/>
+                </div>}
+            </TrackVisibility>
+          </Col>
         </Row>
-        <Row>
-          <img src="../assets/img/icon2.png"></img>
-        </Row>
-      </Container>
+       </Container>
     </section>
   );
 };
