12adcda89efb6cc303fa89232f117a9feab51779
Ben Holgate <holgateb@hotmail.com>
2022-11-23 17:10:59 -0800

    troubleshooting Heroku issues.

--- server.js
+++ server.js
@@ -7,3 +7,10 @@ app.use(cors());
 app.use(express.json());
 app.use("/", router);
 app.listen(5000, () => console.log("Server Running"));
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
