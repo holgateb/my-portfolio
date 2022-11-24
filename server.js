31dd78be440a8fb41428242e7a4ce848ecab0b25
Ben Holgate <holgateb@hotmail.com>
2022-11-23 17:41:41 -0800

    TS.

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/", router);
+// app.use("/", router);
 app.listen(5000, () => console.log("Server Running"));
 
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
